aaf51fcea54bffa9b8132839f262e06c3341d4a2
Akhil Boda <akboda988@gmail.com>
2025-01-09 11:15:56 +1100

    restarted with a new project plan and structure.

---
+++ personalWebsite/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Akhilesh Boda's Personal Website">
+    <meta name="author" content="Akhilesh Boda">
+    <link rel="icon" href="images/ab.jpg" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Akhilesh Boda</title>
+</head>
+<body>
+<div class="d-flex justify-content-center">
+    <nav>
+        <a href="#" class="me-3">MY STORY</a>
+        <a href="#" class="me-3">BODYBUILDING</a>
+        <a href="#" class="me-3">PROFESSIONAL ASPIRATIONS</a>
+        <a href="#" class="me-3">CURRICULUM</a>
+        <a href="#">CONTACT</a>
+    </nav>
+</div>
+<div class="container d-flex flex-column justify-content-center align-items-center vh-100">
+    <h1 class="name">Akhilesh Boda.</h1>
+    <h2 class="name">Welcome to my site!</h2>
+</div>
+</body>
+</html>
